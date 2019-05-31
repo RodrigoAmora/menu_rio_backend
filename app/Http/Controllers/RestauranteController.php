f42cf610cd725e9d9bf06b1bcec17b5d98f68ce9
Rodrigo Amora <rodrigoamora@172-15-0-132.lightspeed.stlsmo.sbcglobal.net>
2019-05-31 14:13:21 -0300

    Ajustes

--- app/Http/Controllers/RestauranteController.php
+++ app/Http/Controllers/RestauranteController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\Service;
 
-class RestauranteController extends Controller
-{
-    //
+class RestauranteController extends Controller {
+    
+    public function oi() {
+    	$service = new Service;
+    	return $service->consultarCoordenatasNoGooglePlace("Rua+amancio+bezerra+250+Iraja");
+    }
 }
--- app/services/Service.php
+++ app/services/Service.php
@@ -1,12 +1,21 @@
 <?php
-namespace App\Http\Services;
+namespace App\Services;
 
 class Service {
 
 	public function consultarCoordenatasNoGooglePlace($endereco) {
-		$googlePlaceAPI = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=rua dos invalidos 198&inputtype=textquery&fields=formatted_address,name,geometry&key=".env("GOOGLE_API_KEY");
+		$googlePlaceAPI = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=".$endereco."&inputtype=textquery&fields=formatted_address,name,geometry&key=".env("GOOGLE_API_KEY");
 
-		return $googlePlaceAPI;
+		$ch = curl_init();
+		curl_setopt_array($ch,
+			[CURLOPT_URL => $googlePlaceAPI,
+			CURLOPT_POST => false]);
+		$p = curl_exec($ch);
+		$json = json_decode($p);
+		curl_close($ch);
+
+		
+		return $json->candidates->formatted_address;//->geometry->location->lat;
 	}
 }
 
