9f0427ff64d972042233915b67bef5beb76844d2
Rodrigo Amora <rodrigoamora@odiaphoto06.local>
2019-05-31 11:58:17 -0300

    Criando o método consultarCoordenatasNoGooglePlace()

--- app/services/Service.php
+++ app/services/Service.php
@@ -2,6 +2,11 @@
 namespace App\Http\Services;
 
 class Service {
-	
+
+	public function consultarCoordenatasNoGooglePlace($endereco) {
+		$googlePlaceAPI = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=rua dos invalidos 198&inputtype=textquery&fields=formatted_address,name,geometry&key=".env("GOOGLE_API_KEY");
+
+		return $googlePlaceAPI;
+	}
 }
 
